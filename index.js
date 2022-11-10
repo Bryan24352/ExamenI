a548ea1ef50786ccb06018550d670ccda3eb1a85
Bryan24352 <bryan.2001322@gmail.com>
2022-11-09 19:06:44 -0600

    Tercer avance

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const  express = require('express');
+const { runInThisContext } = require('vm');
+
+class Server{
+    app = express()
+    port = process.env.port
+
+    constructor(){
+        this.routers();
+    }
+        routers(){
+
+            this.app.evento('/Evento',(req,res) => res.send('Este es el evento del evento'))
+            this.app.fecha('/Fecha',(req,res) => res.send('Esta es la fecha del evento'))
+            this.app.horario('/horario',(req,res) => res.send('Este es el horario del evento'))
+            this.app.lugar('/Lugar',(req,res) => res.send('Este es el lugar del evento'))
+            this.app.Capacidad('/Capacidad',(req,res) => res.send('Esta es la capacidad que tendra el evento'))
+        }
+
+        listen(){
+            this.app.listen(this.port, () => console.log('Servidor HTT port: ${this.port}'))
+        }
+}
+module.exports = Server;
 
